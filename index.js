c8628845d706524b6b4bd67882adebeb66261b0d
choudharyNeera <neerachy97@gmail.com>
2020-07-30 08:54:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var diceLeftImage = "images/dice" + randomNumber1 + ".png";
+var leftImg = document.querySelectorAll("img")[0];
+leftImg.setAttribute("src", diceLeftImage);
+
+randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var diceRightImage = "images/dice" + randomNumber2 + ".png";
+var rightImg = document.querySelectorAll("img")[1];
+rightImg.setAttribute("src", diceRightImage);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML="Player 1 Wins!";
+}else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML="Player 2 Wins!";
+}else{
+  document.querySelector("h1").innerHTML="Draw!";
+}
