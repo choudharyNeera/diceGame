b3ac7361cdb08a4fffa855802a534e0ea413592d
choudharyNeera <neerachy97@gmail.com>
2020-08-03 17:07:16 +0530

    Add click event Listener and change heading to Click me

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var diceLeftImage = "images/dice" + randomNumber1 + ".png";
-var leftImg = document.querySelectorAll("img")[0];
-leftImg.setAttribute("src", diceLeftImage);
+document.body.addEventListener("click", function(){
+  randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var diceLeftImage = "images/dice" + randomNumber1 + ".png";
+  var leftImg = document.querySelectorAll("img")[0];
+  leftImg.setAttribute("src", diceLeftImage);
 
-randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var diceRightImage = "images/dice" + randomNumber2 + ".png";
-var rightImg = document.querySelectorAll("img")[1];
-rightImg.setAttribute("src", diceRightImage);
+  randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var diceRightImage = "images/dice" + randomNumber2 + ".png";
+  var rightImg = document.querySelectorAll("img")[1];
+  rightImg.setAttribute("src", diceRightImage);
 
-if(randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML="Player 1 Wins!";
-}else if(randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML="Player 2 Wins!";
-}else{
-  document.querySelector("h1").innerHTML="Draw!";
-}
+  if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!";
+  }else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins!";
+  }else{
+    document.querySelector("h1").innerHTML="Draw!";
+  }
+})
